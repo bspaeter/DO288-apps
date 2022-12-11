daa5d7a15ffae2919e54a74366b9fbda4deb62e3
Student User <student@workstation.lab.example.com>
2022-12-11 06:54:49 -0500

    command

---
+++ build-app/oc-new-app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /usr/local/etc/ocp4.config
+
+oc new-app --name simple --build-env \
+    npm_config_registry=http://${RHT_OCP4_NEXUS_SERVER}/repository/nodejs \
+    https://github.com/${RHT_OCP4_GITHUB_USER}/DO288-apps \
+    --context-dir build-app
+
